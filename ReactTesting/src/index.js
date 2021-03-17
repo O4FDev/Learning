b879d51ba032849659c588e943d439382e0b2402
Luke <lukelucasdiscord@gmail.com>
2021-03-17 10:43:22 +0000

    Love learning

---
+++ ReactTesting/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import "./style.scss";
+
+import Test from './test';
+
+render(<Test />, document.getElementById('root'));
---
+++ ReactTesting/src/test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// setInterval(tick, 1000);
+
+const Test = () => {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+
+export default Test;
